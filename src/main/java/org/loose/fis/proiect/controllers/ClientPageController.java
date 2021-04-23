59b7bd5e785509b253661582286eb332905b30c5
Nathan Augustinov <nathan.augustinov@student.upt.ro>
2021-04-23 16:10:28 +0300

    APLFIS 26 done

--- src/main/java/org/loose/fis/proiect/controllers/ClientPageController.java
+++ src/main/java/org/loose/fis/proiect/controllers/ClientPageController.java
@@ -1,5 +1,10 @@
 package org.loose.fis.proiect.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ClientPageController
 {
 
@@ -18,5 +23,14 @@ public class ClientPageController
     public void handleSeeProductsListAction()
     {
 
+    }
+
+    public void handleLogoutClientAction() throws Exception
+    {
+        Stage LogoutClient= new Stage();
+        Parent logoutclient = FXMLLoader.load(getClass().getClassLoader().getResource("StartPage.fxml"));
+        LogoutClient.setTitle("StartPage");
+        LogoutClient.setScene(new Scene(logoutclient, 350, 400));
+        LogoutClient.show();
     }
 }
