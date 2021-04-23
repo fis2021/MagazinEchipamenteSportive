3ad25821213bf23fb50ecd174046d79d420b1d22
Paul Alexa <paul.alexa@student.upt.ro>
2021-04-23 16:35:34 +0300

    APLFIS 27 done

--- src/main/java/org/loose/fis/proiect/controllers/ManagerPageController.java
+++ src/main/java/org/loose/fis/proiect/controllers/ManagerPageController.java
@@ -1,5 +1,10 @@
 package org.loose.fis.proiect.controllers;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class ManagerPageController
 
 {
@@ -36,6 +41,16 @@ public class ManagerPageController
     public void handleClientDetailsAction()
     {
 
+    }
+
+
+    public void handleLogoutManagerAction() throws Exception
+    {
+        Stage LogoutManager= new Stage();
+        Parent logoutmanager = FXMLLoader.load(getClass().getClassLoader().getResource("StartPage.fxml"));
+        LogoutManager.setTitle("StartPage");
+        LogoutManager.setScene(new Scene(logoutmanager, 350, 400));
+        LogoutManager.show();
     }
 
 }
