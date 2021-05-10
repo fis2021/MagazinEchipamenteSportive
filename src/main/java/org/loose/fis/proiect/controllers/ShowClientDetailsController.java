fe4b72bbc3586d92a0434ef7a36119dfb0dbf805
Nathan Augustinov <nathan.augustinov@student.upt.ro>
2021-05-10 16:35:06 +0300

    APLFIS 76 done

--- src/main/java/org/loose/fis/proiect/controllers/ShowClientDetailsController.java
+++ src/main/java/org/loose/fis/proiect/controllers/ShowClientDetailsController.java
@@ -7,6 +7,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.loose.fis.proiect.model.Order;
+import org.loose.fis.proiect.model.User;
+
+import static org.loose.fis.proiect.services.FileSystemService.getPathToFile;
 
 public class ShowClientDetailsController
 {
@@ -14,6 +20,18 @@ public class ShowClientDetailsController
     private ListView<String> list = new ListView <String> ();
     @FXML
     private Button BackButton;
+
+    private static ObjectRepository<User> userRepository;
+    private static Nitrite database;
+
+    public static void initDatabase()
+    {
+        database = Nitrite.builder()
+                .filePath(getPathToFile("registration-example.db").toFile())
+                .openOrCreate("test", "test");
+
+        userRepository = database.getRepository(User.class);
+    }
 
     public void handleBackAction() throws  Exception
     {
@@ -32,7 +50,17 @@ public class ShowClientDetailsController
 
     public void set(String s)
     {
-
+        initDatabase();
+        for(User user : userRepository.find())
+        {
+            if(user.getUsername().equals(s))
+            {
+                list.getItems().add("Firstname: " + user.getFirstname());
+                list.getItems().add("Lastname: " + user.getLastname());
+                list.getItems().add("E-mail: " + user.getEmail());
+            }
+        }
+        database.close();
     }
 
     public void cancelShowClientDetails()
