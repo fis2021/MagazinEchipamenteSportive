e52f5e170a8a05a2d0d1e30f31a37e95bff503bc
Nathan Augustinov <nathan.augustinov@student.upt.ro>
2021-05-10 13:37:19 +0300

    APLFIS 75 done

--- src/main/java/org/loose/fis/proiect/controllers/SeeClientDetailsController.java
+++ src/main/java/org/loose/fis/proiect/controllers/SeeClientDetailsController.java
@@ -1,10 +1,28 @@
 package org.loose.fis.proiect.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SeeClientDetailsController
 {
-    public void handleBackAction()
-    {
+    @FXML
+    private Button BackButton;
+    @FXML
+    private Button ShowClientDetails;
 
+
+    public void handleBackAction() throws Exception
+    {
+        Stage Back= new Stage();
+        Parent back = FXMLLoader.load(getClass().getClassLoader().getResource("ManagerPage.fxml"));
+        Back.setTitle("Manager Page");
+        Back.setScene(new Scene(back, 350, 400));
+        Back.show();
+        cancelSeeClientsDetails();
     }
 
     public void handleShowClientDetailsAction()
@@ -15,6 +33,11 @@ public class SeeClientDetailsController
     public void set()
     {
 
+    }
+
+    public void cancelSeeClientsDetails()
+    {
+        BackButton.getScene().getWindow().hide();
     }
 
 }
