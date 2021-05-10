182300530cc3ccf717e9aede7f134caacf547875
Paul Alexa <paul.alexa@student.upt.ro>
2021-05-10 11:48:02 +0300

    APLFIS 70 done

--- src/main/java/org/loose/fis/proiect/controllers/SeeAllOrdersController.java
+++ src/main/java/org/loose/fis/proiect/controllers/SeeAllOrdersController.java
@@ -1,10 +1,31 @@
 package org.loose.fis.proiect.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
 public class SeeAllOrdersController
 {
-    public void handleBackAction ()
+
+    @FXML
+    private Button BackButton;
+    @FXML
+    private Button ShowOrder;
+
+    public void handleBackAction () throws Exception
     {
 
+        Stage Back= new Stage();
+        Parent back = FXMLLoader.load(getClass().getClassLoader().getResource("ManagerPage.fxml"));
+        Back.setTitle("Manager Page");
+        Back.setScene(new Scene(back, 350, 400));
+        Back.show();
+        cancelSeeAllOrders();
     }
 
     public void handleShowOrderAction()
@@ -15,5 +36,11 @@ public class SeeAllOrdersController
     public void set ()
     {
 
+    }
+
+
+    public void cancelSeeAllOrders()
+    {
+        BackButton.getScene().getWindow().hide();
     }
 }
